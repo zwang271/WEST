44c789f469e08235d25ae033198931b9403462dd
Jeremy Sorkin <jsorkin22@wooster.edu>
2022-06-22 23:50:22 -0500

    Pseudo-code implementation for string union

--- MLTL_language/MLTL/main.cpp
+++ MLTL_language/MLTL/main.cpp
@@ -1,11 +1,21 @@
-#include<iostream>
+#include <iostream>
 #include "utils.h"
 
 using namespace std;
 
 int main() {
-	
+
 	cout << string_intersect("", "1ss, sss, sss, sss", 3) << endl;
+
+	string s1[] = {"s1,     ss", "1s,      ss"};
+	vector<string> v1 (s1, s1 + sizeof(s1) / sizeof(string) );
+	string s2[] = {"10  ,  ss     ", "s1,  ss   "};
+	vector<string> v2 (s2, s2 + sizeof(s2) / sizeof(string) );
+	vector<string> v = set_intersect(v1, v2, 2);
+
+	for (int i = 0; i < v.size(); ++i){
+		cout << v[i] << endl;
+	}
 
 	return 0;
 }
--- MLTL_language/MLTL/utils.h
+++ MLTL_language/MLTL/utils.h
@@ -1,33 +1,41 @@
 #pragma once
-#include<string>
-#include "utils.cpp"
+#include <algorithm>
+#include <string>
+//#include "utils.cpp"
+#include <vector>
 
 using namespace std;
 
 string string_intersect(string w_1, string w_2, int n) {
-	w_1.erase(std::remove_if(w_1.begin(),
-		w_1.end(), ::isspace), w_1.end());
-	w_2.erase(std::remove_if(w_2.begin(),
-		w_2.end(), ::isspace), w_2.end());
+	// Remove white-characters from w_1 and w_2
+	w_1.erase(remove_if(w_1.begin(),
+						w_1.end(), ::isspace), w_1.end());
+	w_2.erase(remove_if(w_2.begin(),
+						w_2.end(), ::isspace), w_2.end());
 
+	// If either w_1 or w_2 are empty, return empty.
 	if (w_1 == "" || w_2 == "") {
 		return "";
 	}
 
 	string w = "";
+	// Make w_2 same length as w_1
 	if (w_1.length() > w_2.length()) {
 		int diff = (w_1.length() - w_2.length()) / (n + 1);
 		for (int i = 0; i < diff; i++) {
 			w_2 += ',' + string(n, 's');
 		}
 	}
+	// Make w_1 same length as w_2
 	else if (w_1.length() < w_2.length()) {
 		int diff = (w_2.length() - w_1.length()) / (n + 1);
 		for (int i = 0; i < diff; i++) {
 			w_1 += ',' + string(n, 's');
 		}
 	}
+	// Now w_1.length() == w_2.length()
 
+  // Bit-wise 'and' w_1 and w_2
 	for (int i = 0; i < w_1.length(); i++) {
 		if (w_1[i] != 's' and w_2[i] != 's') {
 			if (w_1[i] != w_2[i]) {
@@ -43,12 +51,45 @@ string string_intersect(string w_1, string w_2, int n) {
 		else {
 			w += w_1[i];
 		}
-			
+
 	}
 
 	return w;
 }
 
 
+vector<string> set_intersect(vector<string> v1, vector<string> v2, int n){
+	vector<string> v = vector<string>();
+
+	// Bit-wise 'and' all entries of v1 and v2
+	for (int i = 0; i < v1.size(); ++i){
+		for (int j = 0; j < v2.size(); ++j){
+			string s = string_intersect(v1[i], v2[j], n);
+			if (s != ""){
+				v.push_back(s);
+			}
+		}
+	}
+
+	return v;
+}
 
 
+vector<string> set_union(vector<string> v1, vector<string> v2){
+	vector<string> v = vector<string>();
+
+	// Union all elements of v1,v2 into v
+	for (int i = 0; i < v1.size(); ++i){
+		for (int j = 0; j < v2.size(); ++j){
+			string s = string_intersect(v1[i], v2[j], n);
+			if (s != ""){
+				v.push_back(s);
+			}
+		}
+	}
+
+
+	// Remove all duplicate entries from v
+
+	return v;
+}
