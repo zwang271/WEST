a26192b965d3da97b48123aa6a30a44517faa350
Zili <ziliwang271@berkeley.edu>
2022-06-27 12:31:02 -0500

    created reg.h and reg.cpp files

---
+++ MLTL_language/MLTL/reg.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "utils.h"
+
+vector<string> set_intersect(vector<string> v1, vector<string> v2, int n);
+vector<string> set_union(vector<string> v1, vector<string> v2, int n);
+vector<string> reg_prop_cons(string s, int n);
+vector<string> reg_prop_var(string s, int n);
+vector<string> reg(string s, int n);
