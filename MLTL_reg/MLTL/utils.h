52e080d95b294e8eb8f7ea850b5da56ffd1a1b00
jelwing <jelwing@umich.edu>
2022-07-18 09:14:23 -0500

    c

--- MLTL_reg/MLTL/utils.h
+++ MLTL_reg/MLTL/utils.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <tuple>
 
 using namespace std;
 
@@ -165,4 +166,4 @@ void print_all_representations(vector<string> v_actual, int n);
 */
 int sum_of_characters(vector<string>);
 
-void print_subformulas(vector< tuple<string, vector<string>> > formulas, int n, string nnf);
+void print_subformulas(vector< tuple<string, vector<string> > > formulas, int n, string nnf);
