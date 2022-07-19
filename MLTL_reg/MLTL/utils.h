49c8ce74443583ec5ba023f1f4e5c51fad3f9908
Zili <ziliwang271@berkeley.edu>
2022-07-19 12:41:49 -0500

    implemented file comparision, ready to compare outputs between reg and bruteforce

--- MLTL_reg/MLTL/utils.h
+++ MLTL_reg/MLTL/utils.h
@@ -167,8 +167,20 @@ void print_all_representations(vector<string> v_actual, int n);
 */
 int sum_of_characters(vector<string>);
 
+
 void print_subformulas(vector< tuple<string, vector<string> > > formulas, int n, string nnf);
+
 
 bool check_vectors_equal(vector<string> *v1, vector<string> *v2, int n);
 
+
+/*
+* Returns the (largest prop var index) + 1 in wff
+*/
 int get_n(string wff);
+
+
+/*
+* Check two files are equal line by line
+*/
+bool compare_files(string f1, string f2);
