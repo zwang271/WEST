82346b1bff91341c576b60a456b7bbda481578b6
Chiara Travesset <ctrave@ctravess.affiliate.iastate.edu>
2022-07-21 15:11:38 -0500

    brute_force_outputs 985-1640

--- MLTL_reg/MLTL/complexity_graph.py
+++ MLTL_reg/MLTL/complexity_graph.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("/Users/ctrave/Downloads/Research/ISU REU 2022/complexities5.txt", sep=" ", header=None, names=["Input Length", "Time(ms)", "Output Length"])
+data = pd.read_csv("/Users/ctrave/Downloads/Research/ISU REU 2022/complexities4.txt", sep=" ", header=None, names=["Input Length", "Time(ms)", "Output Length"])
 print(data)
 #print(data.nlargest(2, "Time(ms)"))
 #print(data.nlargest(3, "Output Length"))
-data.plot.scatter(x="Input Length", y="Output Length", color="Blue")
-#data.plot.scatter(x="Input Length", y="Time(ms)", color="Red")
-plt.title("3 Iterations, 5 Propositional Variables, Delta = 5, Interval Max = 10", y=1.05)
+#data.plot.scatter(x="Input Length", y="Output Length", color="Blue", marker="x")
+data.plot.scatter(x="Input Length", y="Time(ms)", color="Red", marker="x")
+plt.title("1 Iteration, 5 Propositional Variables, Delta = 10, Interval Max = 10", y=1.05)
 plt.show()
