5ae85f8ecce6aeb6d181f3dad93065b330f274da
zwang271 <ziliw1@iastate.edu>
2025-06-26 16:00:27 -0500

    deploying with gunicorn

--- backend/serve.py
+++ backend/serve.py
@@ -37,4 +37,4 @@ def solve(formula):
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(debug=True, host='0.0.0.0') # use this for docker image
+    app.run(debug=True, host='0.0.0.0', port=5000) # use this for docker image
